f81555355cda2886220535893ec49d3040fb41e3
Keyur joshi <keyurjoshi@Keyurs-MacBook-Air.local>
2018-04-29 12:54:47 +0530

    Code changes for db connectivity testing

--- src/main/kotlin/com/finapp/kjoshi/controller/ProductController.kt
+++ src/main/kotlin/com/finapp/kjoshi/controller/ProductController.kt
@@ -3,7 +3,9 @@ package com.finapp.kjoshi.controller
 import com.finapp.kjoshi.bean.ProductBean
 import com.finapp.kjoshi.repository.ProductRepo
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,8 +15,14 @@ class ProductController{
     @Autowired
     lateinit var repo:ProductRepo
 
-    @RequestMapping("/")
+    @RequestMapping("/", method = [(RequestMethod.GET)])
     fun listProducts(): MutableList<ProductBean>? {
         return repo.findAll()
     }
+
+    @RequestMapping("/", method = [(RequestMethod.POST)])
+    fun createProducts(@RequestBody productBean: ProductBean): ProductBean? {
+        return repo.save(productBean)
+    }
+
 }
