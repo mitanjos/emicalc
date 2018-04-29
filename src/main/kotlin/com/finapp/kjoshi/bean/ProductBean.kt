11ed14f2d5d18208e06695a390d00cce312c10c1
Keyur joshi <keyurjoshi@Keyurs-MacBook-Air.local>
2018-04-29 12:47:20 +0530

    Code changes for db connectivity testing

--- src/main/kotlin/com/finapp/kjoshi/bean/ProductBean.kt
+++ src/main/kotlin/com/finapp/kjoshi/bean/ProductBean.kt
@@ -1,10 +1,18 @@
 package com.finapp.kjoshi.bean
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "Product")
 class ProductBean
 {
 
+    @Id
     lateinit var id:Number
 
-
+    @Column
     lateinit var name:String
 }
