00ce0c82d14e4b1dff2d3a50fc1f26c2f28f0d02
Keyur joshi <keyurjoshi@Keyurs-MacBook-Air.local>
2018-04-29 13:18:54 +0530

    Logger added

--- src/main/kotlin/com/finapp/kjoshi/controller/ProductController.kt
+++ src/main/kotlin/com/finapp/kjoshi/controller/ProductController.kt
@@ -2,6 +2,7 @@ package com.finapp.kjoshi.controller
 
 import com.finapp.kjoshi.bean.ProductBean
 import com.finapp.kjoshi.repository.ProductRepo
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,16 +13,20 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/api/product")
 class ProductController{
 
+    private val logger = LoggerFactory.getLogger(ProductController::class.java)
+
     @Autowired
     lateinit var repo:ProductRepo
 
     @RequestMapping("/", method = [(RequestMethod.GET)])
     fun listProducts(): MutableList<ProductBean>? {
+        logger.info("Getting all product list")
         return repo.findAll()
     }
 
     @RequestMapping("/", method = [(RequestMethod.POST)])
     fun createProducts(@RequestBody productBean: ProductBean): ProductBean? {
+        logger.info("Returingin product for productId: {} and productName:{}",productBean.id,productBean.name)
         return repo.save(productBean)
     }
 
