04736ce6f4ea68c356189d75529b8f2acc362451
Keyur joshi <joshikeyur@gmail.com>
2018-05-02 23:17:50 +0530

    Data processing added

--- src/main/kotlin/com/finapp/kjoshi/repository/NAVStgRepo.kt
+++ src/main/kotlin/com/finapp/kjoshi/repository/NAVStgRepo.kt
@@ -2,7 +2,13 @@ package com.finapp.kjoshi.repository
 
 import com.finapp.kjoshi.bean.NAVStgBean
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 
+@Repository
 interface NAVStgRepo:JpaRepository<NAVStgBean,Int>{
-    fun findByAmfiId(amfiId:String):NAVStgRepo
+
+    @Query(value = "select * from nav_stg where amfi_id=:amfiId",nativeQuery = true)
+    fun findByAmfiId(@Param("amfiId")amfiId:String):NAVStgRepo
 }
--- src/main/kotlin/com/finapp/kjoshi/repository/ProductRepo.kt
+++ src/main/kotlin/com/finapp/kjoshi/repository/ProductRepo.kt
@@ -2,5 +2,7 @@ package com.finapp.kjoshi.repository
 
 import com.finapp.kjoshi.bean.ProductBean
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ProductRepo : JpaRepository<ProductBean, Int>
