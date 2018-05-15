a48b4a6015bdbdf521e0da67ce56c1d41355c2cf
Keyur joshi <joshikeyur@gmail.com>
2018-05-15 06:23:09 +0530

    CORS

--- src/main/kotlin/com/finapp/kjoshi/controller/MasterConroller.kt
+++ src/main/kotlin/com/finapp/kjoshi/controller/MasterConroller.kt
@@ -6,13 +6,11 @@ import com.finapp.kjoshi.repository.FundNAVRepo
 import com.finapp.kjoshi.repository.PortfolioRepo
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/navdata/")
+@CrossOrigin
 class MasterConroller{
 
     private val logger = LoggerFactory.getLogger(MasterConroller::class.java)
